fa8f18d5e019f6bdf076153e60dccf2c6fc7ccf5
Aditya <aditya@netroy.in>
2014-06-11 02:36:16 +0200

    fix another race in go-tip

--- cache_test.go
+++ cache_test.go
@@ -43,8 +43,10 @@ func TestExpiration(t *testing.T) {
 	}
 
 	<-time.After(500 * time.Millisecond)
+	cache.mutex.Lock()
 	cache.items["y"].touch(time.Second)
 	item, exists := cache.items["x"]
+	cache.mutex.Unlock()
 	if !exists || item.data != "1" || item.expired() {
 		t.Errorf("Expected `x` to not have expired after 200ms")
 	}
@@ -71,10 +73,12 @@ func TestExpiration(t *testing.T) {
 	}
 
 	<-time.After(600 * time.Millisecond)
+	cache.mutex.RLock()
 	_, exists = cache.items["y"]
 	if exists {
 		t.Errorf("Expected `y` to have expired")
 	}
+	cache.mutex.RUnlock()
 
 	count = cache.Count()
 	if count != 0 {
