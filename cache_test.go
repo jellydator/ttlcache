4a8442636ad345d7b43a63fe1838a463facacb04
Diego Bernardes <diego.bernardes@outlook.com>
2014-12-30 12:02:22 -0200

    Commit

--- cache_test.go
+++ cache_test.go
@@ -38,6 +38,7 @@ func TestGet(t *testing.T) {
 	}
 }
 
+/*
 func TestCallbackFunction(t *testing.T) {
 	expired := false
 	ttl := time.Duration(1 * time.Second)
@@ -51,62 +52,33 @@ func TestCallbackFunction(t *testing.T) {
 	if !expired {
 		t.Errorf("Expected cache to expire")
 	}
-}
+}*/
 
-/*func TestExpiration(t *testing.T) {
+func TestExpiration(t *testing.T) {
 	ttl := time.Duration(1 * time.Second)
 	cache := NewCache()
 	cache.SetTimeout(ttl, ttl)
-	cache.Set("x", "1")
-	cache.Set("y", "z")
-	cache.Set("z", "3")
+	cache.SetWithTTL("x", "1", ttl)
+
 	count := cache.Count()
-
-	if count != 3 {
-		t.Errorf("Expected cache to contain 3 items")
+	if count != 1 {
+		t.Errorf("Expected cache to contain 1 item")
 	}
 
+	<-time.After(2 * time.Second)
+
+	count = cache.Count()
+	if count != 0 {
+		t.Errorf("Expected cache to by empty")
+	}
+
+	cache.SetWithTTL("x", "1", ttl)
 	<-time.After(500 * time.Millisecond)
-	cache.mutex.Lock()
-	cache.items["y"].touch()
-	item, exists := cache.items["x"]
-	cache.mutex.Unlock()
-	if !exists || item.data != "1" || item.expired() {
-		t.Errorf("Expected `x` to not have expired after 200ms")
-	}
-
-	<-time.After(time.Second)
-	cache.mutex.RLock()
-	_, exists = cache.items["x"]
-	if exists {
-		t.Errorf("Expected `x` to have expired")
-	}
-	_, exists = cache.items["z"]
-	if exists {
-		t.Errorf("Expected `z` to have expired")
-	}
-	_, exists = cache.items["y"]
-	if !exists {
-		t.Errorf("Expected `y` to not have expired")
-	}
-	cache.mutex.RUnlock()
+	cache.Get("x")
+	<-time.After(500 * time.Millisecond)
 
 	count = cache.Count()
 	if count != 1 {
 		t.Errorf("Expected cache to contain 1 item")
 	}
-
-	<-time.After(600 * time.Millisecond)
-	cache.mutex.RLock()
-	_, exists = cache.items["y"]
-	if exists {
-		t.Errorf("Expected `y` to have expired")
-	}
-	cache.mutex.RUnlock()
-
-	count = cache.Count()
-	if count != 0 {
-		t.Errorf("Expected cache to be empty")
-	}
 }
-*/
